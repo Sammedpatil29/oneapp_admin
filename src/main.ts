d0db4e9c781dc6e1cf985a83f5f4a5c756f55958
Sammed <sammed.patil29@gmail.com>
2025-12-24 16:33:02 +0530

    web install feature added

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,11 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(() => console.log('SW registered'))
+      .catch(err => console.error('SW error', err));
+  });
+}
