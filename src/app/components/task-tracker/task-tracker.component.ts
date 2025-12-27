0c72d9648a16c188a471a5b4e299891815d5cb7e
Sammed <sammed.patil29@gmail.com>
2025-12-27 08:29:14 +0530

    added listner

--- src/app/components/task-tracker/task-tracker.component.ts
+++ src/app/components/task-tracker/task-tracker.component.ts
@@ -55,10 +55,10 @@ mockApiResponse:any;
   this.initDates();
 }
 
-// @HostListener('window:focus', ['$event'])
-//   onWindowFocus(event: any) {
-//     this.getDashboard();  
-//   }
+@HostListener('window:focus', ['$event'])
+  onWindowFocus(event: any) {
+    this.getDashboard();  
+  }
 
   initDates() {
     const today = new Date();
