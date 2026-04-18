9f534b8da5ddca2774918f090cb506ec0a6df321
Sammed <sammed.patil29@gmail.com>
2026-04-19 00:47:08 +0530

    grocery order component updated

--- src/app/components/grocery-order-details/grocery-order-details.component.ts
+++ src/app/components/grocery-order-details/grocery-order-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
@@ -13,11 +13,15 @@ import { MatSelect, MatOption } from "@angular/material/select";
   templateUrl: './grocery-order-details.component.html',
   styleUrl: './grocery-order-details.component.css'
 })
-export class GroceryOrderDetailsComponent {
+export class GroceryOrderDetailsComponent implements OnInit {
   order: any;
-  riders = ['sudu','kallappa','mallappa']
+  riders:any = []
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<GroceryOrderDetailsComponent>, private ordersService: OrdersService) {
     this.order = data.order;
+  }
+
+  ngOnInit(): void {
+    this.getRiders();
   }
 
   getStatusClass(status: string): string {
@@ -32,7 +36,12 @@ export class GroceryOrderDetailsComponent {
   }
 
   cancelOrder() {
-    // TODO: Implement cancel order logic
+    let params = {
+      "orderId": this.order.id,
+    }
+    this.ordersService.cancelOrder(params).subscribe((res:any)=>{
+      this.dialogRef.close();
+    })
   }
 
   markPacked() {
@@ -45,5 +54,32 @@ export class GroceryOrderDetailsComponent {
     })
   }
 
-  assignRider(rider:any){}
+  getRiders(){
+    this.ordersService.getRiders().subscribe((res:any)=>{
+      let onlineRiders = res
+      this.riders = [];
+      onlineRiders.forEach((rider:any)=>{
+        this.riders.push({
+          id: rider.id,
+          name: rider.name
+        })
+      })
+      console.log(this.riders)
+    })
+    
+  } 
+
+  assignRider(rider:any){
+    let params = {
+      "orderId": this.order.id,
+      "riderId": rider
+    }
+    this.ordersService.assignRider(params).subscribe((res:any)=>{
+      this.order = res.data
+    })
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
 }
--- src/app/components/orders-layout/orders-layout.component.ts
+++ src/app/components/orders-layout/orders-layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import { MatFormField, MatLabel } from "@angular/material/input";
+import { MatDatepickerToggle, MatDatepicker, MatDatepickerModule } from "@angular/material/datepicker";
 
 @Component({
   selector: 'app-orders-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatFormField, MatLabel, MatDatepickerToggle, MatDatepicker, MatDatepickerModule],
   templateUrl: './orders-layout.component.html',
   styleUrl: './orders-layout.component.css'
 })
