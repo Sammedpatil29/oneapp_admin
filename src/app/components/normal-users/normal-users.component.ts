f49a9929c85389ea3b40d734865dd46f3ef787ed
Sammed Patil <sammed.patil29@gmail.com>
2026-05-06 13:21:57 +0530

    new components added

---
+++ src/app/components/normal-users/normal-users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-normal-users',
+  imports: [],
+  templateUrl: './normal-users.component.html',
+  styleUrl: './normal-users.component.css'
+})
+export class NormalUsersComponent {
+
+}
