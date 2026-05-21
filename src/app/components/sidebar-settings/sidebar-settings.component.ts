76896c60eb6e0b8ec45126d06c7a19c68f8eedb4
Sammed <sammed.patil29@gmail.com>
2026-05-21 14:46:46 +0530

    side bar and metadata changed

---
+++ src/app/components/sidebar-settings/sidebar-settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { UnderDevelopmentComponent } from "../under-development/under-development.component";
+
+@Component({
+  selector: 'app-sidebar-settings',
+  imports: [UnderDevelopmentComponent],
+  templateUrl: './sidebar-settings.component.html',
+  styleUrl: './sidebar-settings.component.css'
+})
+export class SidebarSettingsComponent {
+
+}
