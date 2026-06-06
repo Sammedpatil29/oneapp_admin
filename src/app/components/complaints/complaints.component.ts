0bc4e0b946dae3d174a0109a6d56d064477b5f13
Sammed <sammed.patil29@gmail.com>
2026-06-06 23:02:57 +0530

    complaints updates

--- src/app/components/complaints/complaints.component.ts
+++ src/app/components/complaints/complaints.component.ts
@@ -38,6 +38,7 @@ export class ComplaintsComponent implements OnInit, OnChanges {
     ],
     "created_at": "2026-05-20T08:30:00.000Z",
     "closed_at": null,
+    "comment": [],
     "svg": "M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-7 12h-2v-2h2v2zm0-4h-2V6h2v4z"
   },
   {
