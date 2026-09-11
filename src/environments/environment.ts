0ffe3c71dbeea783d9a1b4a5b32c8986a8b89177
Sammed <sammed.patil29@gmail.com>
2026-09-11 20:15:08 +0530

    env fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://oneapp-express-singapore.onrender.com',
   version: "1.0.0 (Pre-Prod/development)"
 };
